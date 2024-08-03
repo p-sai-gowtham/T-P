a79a584c887ec2657f84607f86144cf89313f82d
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2024-08-03 22:18:33 +0530

    add code

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name= 'app'
+
+urlpatterns = [
+    path('',views.home),
+    path("dashboard", views.dashboard),
+    path("adminview", views.Admin, name="admin" ),
+]
---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from user.models import User
+
+def home(request):
+    return render(request, "home/index.html")
+
+@login_required
+def dashboard(request):
+    return render(request, "home/dashboard.html")
+
+@login_required
+def Admin(request):
+    if request.GET.get('page') == 'edit':
+        user_id = request.GET.get('id')
+        user = User.objects.get(pk=user_id)
+        return render(request, 'home/admin.html', {'user': user})
+    users = User.objects.filter(is_superuser=False).values()
+    return render(request, "home/admin.html", {"users":users})
+
